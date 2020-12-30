fd5521bfba10a3a524ebce1ee331e75f73bcbf5a
mikadze <mikadze.davit@gmail.com>
2020-12-30 15:33:35 +0400

    server config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("API_URL", "localhost:1337/api"),
+  url: env("API_URL", "/api"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "4a020bbd706ae2e034f4cb082e5eb89c"),
