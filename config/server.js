ca066b0234e54e38da842f1ad4f6779436d34d09
mikadze <mikadze.davit@gmail.com>
2020-12-30 21:55:37 +0400

    change api link

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("API_URL", "/api"),
+  url: env("API_URL", "/apis"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "4a020bbd706ae2e034f4cb082e5eb89c"),
